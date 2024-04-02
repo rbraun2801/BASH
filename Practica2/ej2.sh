b5fcf6904fb95202735b9264c23c9c3b76b9f8ce
Roberto Braun <robertobraun87@gmail.com>
2024-04-02 20:10:57 -0300

    arreglos ej2

--- Practica2/ej2.sh
+++ Practica2/ej2.sh
@@ -9,13 +9,13 @@ read num2
 if [ $num1 -gt $num2 ]; then
     porc1=$((($num1*20)/100 ))
     porc2=$((($num2*30)/100))
-    echo "El 20% del primer numero es: $porc1"
-    echo "El 30% del segundo numero es: $porc2"
+    echo "El 20% de $num1 numero es: $porc1"
+    echo "El 30% de $num2 numero es: $porc2"
 else
     porc1=$((($num2*20)/100 ))
     porc2=$((($num1*30)/100))
-    echo "El 30% del primer numero es: $porc1"
-    echo "El 20% del segundo numero es: $porc2"
+    echo "El 30% de $num2 es: $porc1"
+    echo "El 20% de $num1 numero es: $porc2"
 fi
 prom=$((($num1+$num2)/2))
 
