f4fa8eb746f6eff3f55cf6f09261fa220dbc9a31
Roberto Braun <robertobraun87@gmail.com>
2024-04-02 20:38:31 -0300

    modificacion ej2

--- Practica2/ej2.sh
+++ Practica2/ej2.sh
@@ -17,6 +17,6 @@ else
     echo "El 20% de $num2 es: $porc1"
     echo "El 30% de $num1 numero es: $porc2"
 fi
-prom=$((($num1+$num2)/2))
+prom=$(echo "scale=2; ($num1+$num2)/2" | bc)
 
 echo "El promedio de ambos numeros es: $prom"
