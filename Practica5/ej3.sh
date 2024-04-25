5b11f8939fd10a44eb975fe13a59620faef3dfff
roberto <robertobraun87@gmail.com>
2024-04-25 14:36:34 -0300

    ej3 practica5 con tar

--- Practica5/ej3.sh
+++ Practica5/ej3.sh
@@ -3,8 +3,18 @@
 mkdir -p archivosprueba
 #cambio de carpeta
 cd archivosprueba
-#creo los archivos
-touch archivo{1..40}.txt
+tar -xvf pares.tar .
+#creo los archivos y dentro del if analizo si es par o impar, lo creo y guardo
+#los pares en un tar llamado pares
+for ((i=0;i<40;i++)); do
+     if (( $i % 2 ==0 )); then
+          touch archivo$i.txt
+          tar -rvf pares.tar archivo$i.txt
+     else
+          touch archivo$i.txt
+     fi
+done
+
 #Realizo un ls para listar grep para seleccionar los que contengan un 3 
 #utilizo el for con el listado que selecciona todos los 3 hago un mv para reemplazar solo la extension del archivo
 for arch in $(ls | grep 3)  ; do
